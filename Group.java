14c66d94c39fe700cb81b4a772d57148a5e79835
Karin <KarinQian2003@gmail.com>
2020-05-17 20:02:19 -0400

    modified constructor

--- Group.java
+++ Group.java
@@ -9,23 +9,11 @@ public class Group {
 	HashMap<String, Integer> elements = new HashMap<String, Integer>();
 	
 	//constructor for molecules you get from GUI
-	public Group (int numHydrogen, int numCarbons, int x, int y) {
+	public Group (int numHydrogen, int numCarbons) {
 		name = Nomenclature.oPrefixes.get(numCarbons)+"yl";
 		elements.put("hydrogen", numHydrogen);
 		elements.put("carbon", numCarbons);
-		
-		/*
-		 * insert some grade 9 math to calculate row/column of the group
-		 */
 	}
-	
-	//debugging constructor
-	public Group(int h, int c) {
-		name = Nomenclature.oPrefixes.get(c)+"yl";
-		elements.put("hydrogen", h);
-		elements.put("carbon", c);
-	}
-	
 	//constructor for bonds
 	public Group (String bondType) {
 		name = bondType;
